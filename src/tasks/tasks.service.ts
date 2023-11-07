a90290357899dd3eb4b95590470f28dfb44582cc
cyblogerz <pranavajay2002@gmail.com>
2023-11-08 01:26:00 +0530

    Added deleting and fetching tasksby id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { randomUUID } from 'crypto';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -10,7 +11,17 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskByid(id: string) {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTaskbyId(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id != id);
+    return this.tasks;
+  }
+
+  createTask(_createTaskDto: CreateTaskDto): Task {
+    const { title, description } = _createTaskDto;
     const task: Task = {
       id: randomUUID(),
       title,
