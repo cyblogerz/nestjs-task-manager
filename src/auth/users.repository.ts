34b678141cbb2c3053de6f8afc2374d88d38c988
cyblogerz <pranavajay2002@gmail.com>
2023-11-12 16:00:38 +0530

    Basic authentication

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UserRepository {
@@ -9,4 +10,14 @@ export class UserRepository {
     @InjectRepository(User)
     private readonly userEntityRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    //save the user to db
+    const user = this.userEntityRepository.create({
+      username,
+      password,
+    });
+    await this.userEntityRepository.save(user);
+  }
 }
